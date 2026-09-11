7ecf6b8fcdfc46367fa6954bc0a292b38381a2e9
Dhruv_S <dhruvshankpal@gmail.com>
2026-09-11 14:33:28 -0700

    Update assets.js to modify backgrounds and add stickers

--- public/assets.js
+++ public/assets.js
@@ -10,10 +10,20 @@
 //   Groups: support, what, rage, said, useless. Anything else makes its own row.
 
 window.YAP_ASSETS = {
-  backgrounds: [
-    // { id: 'desert', label: 'desert', src: 'backgrounds/desert.png', soil: 'none' },
-  ],
+  backgrounds: [],
   stickers: [
-    // { id: 'shark', label: 'shark', src: 'stickers/shark.png', group: 'support' },
+    { id: 'Social battery lol', label: 'Social battery lol', src: 'Social battery lol.jpeg'},
+    { id: '_ (10)', label: '_ (10)', src: '_ (10).jpeg'},
+    { id: '_ (11)', label: '_ (11)', src: '_ (11).jpeg'},
+    { id: '_ (12)', label: '_ (12)', src: '_ (12).jpeg'},
+    { id: '_ (2)', label: '_ (2)', src: '_ (2).jpeg'},
+    { id: '_ (3)', label: '_ (3)', src: '_ (3).jpeg'},
+    { id: '_ (4)', label: '_ (4)', src: '_ (4).jpeg'},
+    { id: '_ (5)', label: '_ (5)', src: '_ (5).jpeg'},
+    { id: '_ (6)', label: '_ (6)', src: '_ (6).jpeg'},
+    { id: '_ (7)', label: '_ (7)', src: '_ (7).jpeg'},
+    { id: '_ (8)', label: '_ (8)', src: '_ (8).jpeg'},
+    { id: 'im back yall', label: 'im back yall', src: 'im back yall.jpeg'},
+    { id: 'pixel art meme cat', label: 'pixel art meme cat', src: 'pixel art meme cat.jpeg'},
   ],
 };
